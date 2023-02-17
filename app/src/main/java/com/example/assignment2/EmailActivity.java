e9fa5b65cd7d724a82305533cb4578623ed8dd5b
vy2000eng <vy2000eng@gmail.com>
2023-02-17 11:40:36 -0500

    made app bar, need to finish intent for email

---
+++ app/src/main/java/com/example/assignment2/EmailActivity.java
@@ -0,0 +1,6 @@
+package com.example.assignment2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EmailActivity extends AppCompatActivity {
+}
--- app/src/main/java/com/example/assignment2/MainActivity.java
+++ app/src/main/java/com/example/assignment2/MainActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
@@ -12,19 +16,24 @@ import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+
+import androidx.appcompat.widget.Toolbar;
+
 public class MainActivity extends AppCompatActivity {
-    RadioButton averageCase,WorstCase;
+    RadioButton averageCase, WorstCase;
     int whichArray = 0;
     int whichDataStructure = -1;
+    private static final int CLICKED_ICON = R.drawable.email_icon;
+    boolean checkToSendEmail = false;
 
-    String [][] worstCaseArray = {
-             {"Get Min: O(log(n))", "Insert: O(log(n))", "Search: O(log(n))"},//2-3 tree
-             {"Get Min: O(n)", "Insert: O(n)", "Search: O(n)"},        //bst
-             {"Get Min: O(n)", "Insert: O(n)", "Search: O(n)"},        //hash-table
-             {"Get Min: O(n)", "Insert: O(1)", "Search: O(n)"},         //linked-list
-             {"Get Min: O(1)", "Insert: O(log(n))", "Search: O(n)"}                  //min-heap
+    String[][] worstCaseArray = {
+            {"Get Min: O(log(n))", "Insert: O(log(n))", "Search: O(log(n))"},//2-3 tree
+            {"Get Min: O(n)", "Insert: O(n)", "Search: O(n)"},        //bst
+            {"Get Min: O(n)", "Insert: O(n)", "Search: O(n)"},        //hash-table
+            {"Get Min: O(n)", "Insert: O(1)", "Search: O(n)"},         //linked-list
+            {"Get Min: O(1)", "Insert: O(log(n))", "Search: O(n)"}                  //min-heap
     };
-    String [][] averageCaseArray = {
+    String[][] averageCaseArray = {
             {"Get Min: O(log(n))", "Insert: O(log(n))", "Search: O(log(n))"},//2-3 tree
             {"Get Min: O(log(n))", "Insert: O(log(n))", "Search: O(log(n))"},        //bst
             {"Get Min: O(1)", "Insert: O(1)", "Search: O(1)"},        //hash-table
@@ -34,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
 
         Log.d("life_cycle", "onCreate invoked");
         super.onCreate(savedInstanceState);
@@ -45,11 +55,31 @@ public class MainActivity extends AppCompatActivity {
                 android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+        Toolbar myToolBar = findViewById(R.id.toolbar);
+        setSupportActionBar(myToolBar);
 
 
     }
 
-    public void buttonClick(View Button){
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_activity, menu);
+        MenuItem menuItem = menu.findItem(R.id.menu_save);
+         menuItem.setOnMenuItemClickListener(item -> {
+            checkToSendEmail = true;
+
+            menuItem.setIcon(CLICKED_ICON);
+            buttonClick(item.getActionView());
+            return true;
+        });
+
+        return true;
+    }
+
+
+
+    public void buttonClick(View Button) {
         EditText EmailAddress = findViewById(R.id.editTextTextEmailAddress2);
         EditText SubjectOfEmailAddress = findViewById(R.id.editTextTextEmailAddress3);
         Spinner dataStructure = findViewById(R.id.dataStructureSpinner);
@@ -59,32 +89,32 @@ public class MainActivity extends AppCompatActivity {
         boolean getMinCheckBox = ((CheckBox) findViewById(R.id.getMinCheckBox)).isChecked();
         boolean insertCheckBox = ((CheckBox) findViewById(R.id.insertCheckBox)).isChecked();
         boolean searchCheckBox = ((CheckBox) findViewById(R.id.searchCheckBox)).isChecked();
-        boolean [] whichCheckBoxes = {getMinCheckBox, insertCheckBox,searchCheckBox};
+        boolean[] whichCheckBoxes = {getMinCheckBox, insertCheckBox, searchCheckBox};
 
 
-        String contentOfEmailAddress = "To: " +EmailAddress.getText().toString();
+        String contentOfEmailAddress = "To: " + EmailAddress.getText().toString();
         String contentOfSubjectOfEmailAddress = "Subject: " + SubjectOfEmailAddress.getText().toString();
         String prefixForMessage = "Time Complexity for ";
-        String contentOfDataStructure =  dataStructure.getSelectedItem().toString();
-        String runningTime = (averageCase.isChecked())?"Average Case ":(WorstCase.isChecked())?"Worst Case ":"";
+        String contentOfDataStructure = dataStructure.getSelectedItem().toString();
+        String runningTime = (averageCase.isChecked()) ? "Average Case " : (WorstCase.isChecked()) ? "Worst Case " : "";
         StringBuilder outputOfSelectedOperations = new StringBuilder();
-        whichArray = (averageCase.isChecked())?0:1;
-        String[][] currentArray = whichArray == 0? averageCaseArray:worstCaseArray;
-        whichDataStructure = contentOfDataStructure.equals("2–3 Tree") ? 0:
-                             contentOfDataStructure.equals("Binary Search Tree")?1:
-                                     contentOfDataStructure.equals("Hash Table")?2:
-                                             contentOfDataStructure.equals("Linked List")?3:4;
+        whichArray = (averageCase.isChecked()) ? 0 : 1;
+        String[][] currentArray = whichArray == 0 ? averageCaseArray : worstCaseArray;
+        whichDataStructure = contentOfDataStructure.equals("2–3 Tree") ? 0 :
+                contentOfDataStructure.equals("Binary Search Tree") ? 1 :
+                        contentOfDataStructure.equals("Hash Table") ? 2 :
+                                contentOfDataStructure.equals("Linked List") ? 3 : 4;
 
-        for(int i =0; i < whichCheckBoxes.length; i++){
-            if(whichCheckBoxes[i]){
+        for (int i = 0; i < whichCheckBoxes.length; i++) {
+            if (whichCheckBoxes[i]) {
                 outputOfSelectedOperations.append("\t").append(currentArray[whichDataStructure][i]).append('\n');
             }
         }
         String contentOfMessage =
-                runningTime+
-                prefixForMessage+
-                contentOfDataStructure +
-                        ":"+ '\n'
+                runningTime +
+                        prefixForMessage +
+                        contentOfDataStructure +
+                        ":" + '\n'
                         + outputOfSelectedOperations;
 
 
@@ -92,7 +122,7 @@ public class MainActivity extends AppCompatActivity {
         TextView setContentOfSubjectOfEmailAddress = findViewById(R.id.whatsItAbout);
         TextView setContentOfDataStructure = findViewById(R.id.content);
 
-        setContentOfEmailAddress.setText( contentOfEmailAddress);
+        setContentOfEmailAddress.setText(contentOfEmailAddress);
         setContentOfSubjectOfEmailAddress.setText(contentOfSubjectOfEmailAddress);
         setContentOfDataStructure.setText(contentOfMessage);
 
